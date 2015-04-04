c4cc36d62b066972033aecf38f5509b6a1c05c90
Mattias Harrysson <mathar@kth.se>
2015-04-04 14:56:52 +0200

    update assets

--- src/assets.hpp
+++ src/assets.hpp
@@ -1,14 +1,9 @@
 #ifndef ASSETS_HPP_INCLUDED
 #define ASSETS_HPP_INCLUDED
 
-#define BASIC_VS "assets/shaders/basic.vs"
-#define BASIC_FS "assets/shaders/basic.fs"
 #define CHEAP_VS "assets/shaders/cheap.vs"
+#define BASIC_FS "assets/shaders/basic.fs"
 
-#define GRASS_PNG "assets/textures/grass.png"
-#define TREE1_PNG "assets/textures/tree1.png"
-#define TREE2_PNG "assets/textures/tree2.png"
-#define TREE3_PNG "assets/textures/tree3.png"
-#define TREE4_PNG "assets/textures/tree4.png"
+#define PEPPER_PNG "assets/images/pepper.png"
 
 #endif
